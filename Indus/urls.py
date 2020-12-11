2e7a998d345ba6d82eaa792b90b1c097f8e6ea8b
Pointdexter99 <49776332+Pointdexter99@users.noreply.github.com>
2020-12-12 00:21:43 +0530

    Update urls.py

--- Indus/urls.py
+++ Indus/urls.py
@@ -1,31 +1,16 @@
-"""Indus URL Configuration
+from django.urls import path
+from users import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic.base import TemplateView
-from django.contrib.auth import views as auth_views
+app_name = 'users'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('feeds/', include('feeds.urls')),
-    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='plain/text')),
-    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='plain/text')),
-    path('', include('users.urls')),
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name ="users/password_reset.html" ),name ="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name ="users/password_confirm.html"), name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name ="users/password_form.html"), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name ="users/password_done.html"), name="password_reset_complete"),
+    path('', views.signup, name='signup'),
+    path('signup_message', views.signup_message, name='signup_message'),
+    path('activate/<uid64>/<token>', views.activate_account, name='activate'),
+    path('login/', views.user_login,name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('user/interests/', views.interests, name='interests'),
+    path('user/profile/', views.profile, name='profile'),
+    path('feedback/', views.feedback, name='feedback'),
+    path('help', views.help, name='help')
 ]
